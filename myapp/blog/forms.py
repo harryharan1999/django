7a95886a07e8e55e179c682a37ae838c34b1e06f
HarryharanAJ <155060539+harryharan1999@users.noreply.github.com>
2024-07-22 22:19:23 +0530

    Delete myapp directory

--- myapp/blog/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactFormModelForm(forms.Form):
-    name= forms.CharField(label='name', max_length=100,required=True)
-    email = forms.EmailField(label='email', required=True)
-    message = forms.CharField(label='message', required=True,widget=forms.Textarea)
